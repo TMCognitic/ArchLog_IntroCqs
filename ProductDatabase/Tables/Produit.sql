d7270221a592df90483f887533cba85430d57561
Thierry Morre <thierry.morre@cognitic.be>
2026-01-15 12:00:49 +0100

    Ajoutez des fichiers projet.

---
+++ ProductDatabase/Tables/Produit.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Produit]
+(
+	[Id] INT NOT NULL IDENTITY, 
+	[Nom] NVARCHAR(75) NOT NULL,
+	[Prix] FLOAT NOT NULL,
+    CONSTRAINT [PK_Produit] PRIMARY KEY ([Id]),
+	CONSTRAINT [CK_Produit_Prix] CHECK ([Prix] >= 0)
+)
